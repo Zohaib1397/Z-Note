6e31447d6fea2575ab72b2d0ab61b36e15360eb3
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2021-09-27 21:37:59 +0500

    Added Card Background in NoteCard

--- app/src/main/java/com/example/z_note/feature/presentation/UI/NoteCard.kt
+++ app/src/main/java/com/example/z_note/feature/presentation/UI/NoteCard.kt
@@ -5,14 +5,41 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.z_note.ui.theme.ZNoteTheme
 
+
+/*
+ Note State (Expanded / Collapsed)
+
+* To determine the state of note following enum class
+* contains two states
+* if note is expanded the state will be NoteState.Expanded
+* and if note is not expanded the state will be NoteState.Collapsed
+* */
 private enum class NoteState{
     Collapsed,
     Expanded
 }
+
+/*
+* Note Card
+*
+* following NoteCard Composable function contains only states
+* 1) the Expanded State for a note
+* 2) Little Arrow head rotation state based on the expanding state of
+*    note
+*
+* This NoteCard calls another Composable which accepts the parameters of states
+* and onChangeState is to change the current state to either Expanded or Collapsed
+* -- here Lambda functions are useful
+* */
 
 
 @Composable
@@ -33,9 +60,54 @@ fun NoteCard() {
         }
 
     }
+    NoteCardView(
+        noteState,
+        arrowRotate
+    ){
+        noteState = when(noteState){
+            NoteState.Collapsed -> NoteState.Expanded
+            NoteState.Expanded -> NoteState.Collapsed
+        }
+    }
 }
 
+/*
+* Following Composable just contains the view
+*
+* NoteCardView is a background Card having the elevation and RoundedShape
+* */
 
+@Composable
+private fun NoteCardView(
+    noteState: NoteState,
+    arrowRotate: Float,
+    modifier:Modifier = Modifier,
+    onNoteStateChange:() -> Unit
+) {
+    Card(
+        modifier = modifier.clickable{
+            onNoteStateChange()
+        },
+        shape = RoundedCornerShape(12.dp),
+        elevation = 2.dp
+    ) {
+        NoteColumnView(
+            noteState,
+            arrowRotate,
+        ){
+            onNoteStateChange()
+        }
+    }
+}
+
+@Composable
+private fun NoteColumnView(
+    noteState: NoteState,
+    arrowRotate: Float,
+    function: () -> Unit
+) {
+
+}
 
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES,

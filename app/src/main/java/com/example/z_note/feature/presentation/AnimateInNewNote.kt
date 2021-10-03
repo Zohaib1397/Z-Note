f526de55567b22c200befb0f8bbf03372e40791c
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2021-10-03 20:17:42 +0500

    Added the starting point of Note animation

--- app/src/main/java/com/example/z_note/feature/presentation/AnimateInNewNote.kt
+++ app/src/main/java/com/example/z_note/feature/presentation/AnimateInNewNote.kt
@@ -1,11 +1,50 @@
 package com.example.z_note.feature.presentation
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.keyframes
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.IntSize
 import com.example.z_note.feature.presentation.States.NoteState
+import com.example.z_note.feature.presentation.UI.AddNote
+import com.example.z_note.feature.presentation.notes.NoteViewModel
 
+@ExperimentalAnimationApi
 @Composable
 fun AnimateNewNote(
-    noteState: NoteState
+    addNoteTitle:String,
+    addNoteContent:String,
+    viewModel:NoteViewModel,
+    noteState: NoteState,
+    releventComposable: @Composable () -> Unit
 ) {
-
+    AnimatedContent(
+        targetState =  noteState,
+        transitionSpec = {
+            fadeIn() with
+            fadeOut() using
+                    SizeTransform{ initialSize, targetSize ->
+                        if(targetState==NoteState.Expanded){
+                            keyframes{
+                                IntSize(targetSize.width,initialSize.height) at 150
+                                durationMillis = 300
+                            }
+                        } else {
+                            keyframes{
+                                IntSize(initialSize.width,targetSize.height) at 150
+                                durationMillis = 300
+                            }
+                        }
+                    }
+        }
+    ){ targetState ->
+        if(targetState == NoteState.Collapsed){
+            releventComposable()
+        }else {
+            AddNote(
+                title = addNoteTitle,
+                content = addNoteContent,
+                viewModel = viewModel
+            )
+        }
+    }
 }
